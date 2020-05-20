2a3c5938d60f6860a238e71068ade386e4cbbb64
Joao Trindade <jtrindade@cc.isel.ipl.pt>
2020-05-20 13:49:45 +0100

    18: parallel sum

--- 18 - thread pool/adder.c
+++ 18 - thread pool/adder.c
@@ -4,8 +4,14 @@
 #include <assert.h>
 #include "chrono.h"
 
+#include "tpool.h"
+#include "countdownlatch.h"
+
+#define NTHREADS 4
 #define NITERS 50
-#define NVALS 50000
+#define NVALS 5000000
+
+tpool_t tpool;
 
 int vals[NVALS];
 
@@ -18,6 +24,95 @@ long seq_adder(int vals[], size_t nvals) {
 	long total = 0;
 	for (size_t i = 0; i < nvals; ++i)
 		total += vals[i];
+	return total;
+}
+
+typedef struct sum_task {
+	cdlatch_t * latch;
+	int * pini;
+	int * pend;
+	long result;
+} sum_task_t;
+
+void sum_task_func1(void * ptr) {
+	sum_task_t * sum_task = (sum_task_t *)ptr;
+	long total = 0;
+	for (int * p = sum_task->pini; p < sum_task->pend; p += NTHREADS) {
+		total += *p;
+	}
+	sum_task->result = total;
+	cdlatch_count_down(sum_task->latch);
+}
+
+long par_adder1(int vals[], size_t nvals) {
+	cdlatch_t latch;
+	cdlatch_init(&latch, NTHREADS);
+	
+	sum_task_t sum_tasks[NTHREADS];	
+	for (int i = 0; i < NTHREADS; ++i) {
+		sum_tasks[i].latch = &latch;
+		sum_tasks[i].pini = &vals[i];
+		sum_tasks[i].pend = &vals[nvals];
+		
+		tp_submit(&tpool, sum_task_func1, &sum_tasks[i]);
+	}
+	
+	cdlatch_await(&latch);
+	
+	long total = 0;
+	for (int i = 0; i < NTHREADS; ++i) {
+		total += sum_tasks[i].result;
+	}
+	
+	return total;
+}
+
+/*
+typedef struct sum_task {
+	cdlatch_t * latch;
+	int * pini;
+	int * pend;
+	long result;
+} sum_task_t;
+*/
+
+void sum_task_func2(void * ptr) {
+	sum_task_t * sum_task = (sum_task_t *)ptr;
+	long total = 0;
+	for (int * p = sum_task->pini; p < sum_task->pend; ++p) {
+		total += *p;
+	}
+	sum_task->result = total;
+	cdlatch_count_down(sum_task->latch);
+}
+
+long par_adder2(int vals[], size_t nvals) {
+	cdlatch_t latch;
+	cdlatch_init(&latch, NTHREADS);
+	
+	int region_size = (nvals + (NTHREADS - 1)) / NTHREADS;
+	
+	sum_task_t sum_tasks[NTHREADS];
+	for (int i = 0; i < NTHREADS; ++i) {
+		int * pini = &vals[i * region_size];
+		int * pend = (pini + region_size) < &vals[nvals] ?
+			(pini + region_size) :
+			&vals[nvals];
+		
+		sum_tasks[i].latch = &latch;
+		sum_tasks[i].pini = pini;
+		sum_tasks[i].pend = pend;
+
+		tp_submit(&tpool, sum_task_func2, &sum_tasks[i]);
+	}
+	
+	cdlatch_await(&latch);
+	
+	long total = 0;
+	for (int i = 0; i < NTHREADS; ++i) {
+		total += sum_tasks[i].result;
+	}
+	
 	return total;
 }
 
@@ -47,9 +142,14 @@ void test(char *name,
 }
 
 int main() {
+	tp_init(&tpool, NTHREADS);
+	
 	init_vals();
 	 
-	test("sequential adder", seq_adder, vals, NVALS, NVALS);
+	test("sequential adder", seq_adder,  vals, NVALS, NVALS);
+	test("parallel#1 adder", par_adder1, vals, NVALS, NVALS);
+	test("parallel#2 adder", par_adder2, vals, NVALS, NVALS);
 	
+	// tp_destroy(&tpool);
 	return 0;
 }
